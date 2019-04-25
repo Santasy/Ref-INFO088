d63374a55fcc41434c4aaab39ce0d0c1ea33a5ab
Sebastián Pacheco <44266708+Santasy@users.noreply.github.com>
2019-04-25 03:54:46 -0400

    Update Ejercicio2.cpp

--- 2019 - 04 - 25: Clase 2/Ejercicio2.cpp
+++ 2019 - 04 - 25: Clase 2/Ejercicio2.cpp
@@ -6,25 +6,25 @@
 
 using namespace std;
 
-#define N_C 100 //Número de cálculos a almacenar
+#define N_C 100 //NÃºmero de cÃ¡lculos a almacenar
 
 /*{ retorno} */ void fibonacci(/* *{argumentos} */);
 void printAll(/* *{argumentos} */);
 
 int main(int argc, char **argv){
 	srand(time(NULL));
-	cout.precision(0); // Fija la cantidad de indices decimales a imprimir a través de std::cout a 0, asegurando que el número entero imprima todos sus dígitos.
-	// Para que funcione, se debe entregar '<< fixed' antes de entregar el número '<< n'
+	cout.precision(0); // Fija la cantidad de indices decimales a imprimir a travÃ©s de std::cout a 0, asegurando que el nÃºmero entero imprima todos sus dÃ­gitos.
+	// Para que funcione, se debe entregar '<< fixed' antes de entregar el nÃºmero '<< n'
 	
-	int calculos = 0; // Contador de cálculos necesitados durante la ejecución del programa
-	/*{tipo}*/ *calculados = new /*{tipo}*/[N_C]{0, 1, 1}; // Nos ahorramos los primeros cálculos, que son un poco más tedioso de generar.
+	int calculos = 0; // Contador de cÃ¡lculos necesitados durante la ejecuciÃ³n del programa
+	/*{tipo}*/ *calculados = new /*{tipo}*/[N_C]{0, 1, 1}; // Nos ahorramos los primeros cÃ¡lculos, que son un poco mÃ¡s tedioso de generar.
 	
-	// Esta es una técnica lógica simple, que nos permiten crear condiciones para el avance de nuestro programa
-	for(int i = 3; i < N_C; i++) calculados[i] = -1; // Definimos que si la casilla contiene un '-1', significará que el valor no ha sido calculado.
+	// Esta es una tÃ©cnica lÃ³gica simple, que nos permiten crear condiciones para el avance de nuestro programa
+	for(int i = 3; i < N_C; i++) calculados[i] = -1; // Definimos que si la casilla contiene un '-1', significarÃ¡ que el valor no ha sido calculado.
 	
 	/* Llamdos triviales a la funcion */
 	for(int i = 0; i < 15; i++){
-		int v = rand() % N_C; // Indice a buscar
+		int v = rand() % (N_C * 2); // Indice a buscar
 		/*{tipo}*/ r = fibonacci(/* *{argumentos} */);
 		cout << "Fibonacci indice " << v << ": " << fixed << r << '\n';
 	}
@@ -35,10 +35,10 @@ int main(int argc, char **argv){
 }
 
 /*{retorno}*/ fibonacci(/* *{argumentos} */){
-	/* Función que entrega el número en índice 'i' de la serie de Fibonacci */
+	/* FunciÃ³n que entrega el nÃºmero en Ã­ndice 'i' de la serie de Fibonacci */
 	
 	/*
-	Código
+	CÃ³digo
 	*/
 }
 
